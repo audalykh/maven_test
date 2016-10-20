c5c1307e8ed1fd6c07691fd3614001cb46eae208
Alexey Udalykh <alexey.udaiykh@gteamstaff.com>
2016-10-20 09:39:26 +0800

    Added the structure

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,18 @@
+package com.mycompany.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void shouldPass() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void shouldFail() {
+        assertTrue(false);
+    }
+}
